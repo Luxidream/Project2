32cacf8f221bb47dde7178040faadf3b4e1b69d0
Dzuy Nguyen <dzuy.d.nguyen-1@ou.edu>
2019-03-02 19:39:41 -0600

    Further testing, changed StID

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main
 {
 	public static void main(String[] args) throws IOException 
 	{
-		String StID = "OKCE";
+		String StID = "CDDE";
 		MesoInherit mesoinherit = new MesoInherit(new MesoStation(StID));
 		
 		System.out.println("Ascii Ceiling is: " + mesoinherit.getAvg()[0]
