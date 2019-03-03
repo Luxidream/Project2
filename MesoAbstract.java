53f60d65f8097220987c9856b9364d80aea60f09
Dzuy Nguyen <dzuy.d.nguyen-1@ou.edu>
2019-03-02 19:33:16 -0600

    Commenting MesoAbstract

--- MesoAbstract.java
+++ MesoAbstract.java
@@ -1,12 +1,14 @@
 public abstract class MesoAbstract 
 {
 	protected String MesoStat;
+	//We don't care about any station length that isn't 4
 	final int STATION_LENGTH = 4;
 	
    public MesoAbstract(MesoStation meso) 
    {
 	 MesoStat = meso.getStID();  
    }
+   
    public abstract int[] getAvg();
    public abstract char getLetter();
 
